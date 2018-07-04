d57e9fa2f49f11196e2909b75d0adc18df3ad472
Vitaliy Sheanov <vit.onix@gmail.com>
2018-07-04 23:05:21 +0300

    -

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,11 @@
 import org.gradle.api.internal.HasConvention
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.net.URI
 
 plugins {
     kotlin("jvm") version "1.2.50"
+    id("com.jfrog.bintray") version "1.8.1"
 }
 
 group = "net.arwix.astronomy2"
@@ -11,6 +13,10 @@ version = "0.1"
 
 repositories {
     mavenCentral()
+    jcenter()
+    maven {
+       url = URI("http://dl.bintray.com/v170nix/astronomy2")
+    }
 }
 
 dependencies {
